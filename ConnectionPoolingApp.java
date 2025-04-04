19b46beb5794b197625513d5f4c475fc4d9cd2f2
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-04-04 18:40:32 +0530

    Add files via upload

---
+++ ConnectionPoolingApp.java
@@ -0,0 +1,41 @@
+package in.abc.main;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+
+public class ConnectionPoolingApp {
+
+	public static void main(String[] args) throws SQLException {
+
+		// Creating a pooled connection object
+		MysqlConnectionPoolDataSource dataSource = new MysqlConnectionPoolDataSource();
+
+		dataSource.setUrl("jdbc:mysql://localhost:3306/abc");
+		dataSource.setUser("root");
+		dataSource.setPassword("root");
+
+		// logical connection bought from connection pool
+		Connection connection = dataSource.getConnection(); 
+
+		Statement statement = connection.createStatement();
+
+		ResultSet resultSet = statement.executeQuery("select sid,sname,saddr from student");
+
+		System.out.println("SID\tSNAME\tSADDRES");
+		System.out.println("==========================");
+		while (resultSet.next()) {
+			int sid = resultSet.getInt(1);
+			String sname = resultSet.getString(2);
+			String saddress = resultSet.getString(3);
+			System.out.println(sid + "\t" + sname + "\t" + saddress);
+		}
+		
+		//sending the connection object back to connection pool
+		connection.close();
+	}
+
+}
